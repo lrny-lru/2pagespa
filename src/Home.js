415b33927e3ca3847dace0f7afdcd7b03ec1e872
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2021-12-13 13:50:42 -0500

    first commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Uairean tha mi ’m beachd gu bheil bad sònraichte ann, a tha ceangailte ris an eanchainn chorparra, far a bheil nitheigin a nì an aon obair is a nì crann  rèidio,  a  thogas  smuaintean  mar  a  thogas  crann  rèidio  briathran duinn.  Ach  dhealbhainn  an  ceangal  fhèin  ris  an  eanchainn  mar  a dhealbhainn  ceò  ri  lasair-theine  ...  Mur  biodh  an  crann  ann  am  f ìor
+          ghleus, cha chluinneamaid lid ged a bhiodh an iarmailt làn ghuthan. Rud a tha.</p>
+
+        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
